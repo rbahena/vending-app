5faef7e441a1b8b422350dfbb30a0425f43a2538
Ramiro Bahena <ramiro.bahena@ceneval.edu.mx>
2024-01-29 17:35:21 -0600

    instalar bootstrap en el proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path:'auth',
     loadChildren:()=> import('./modules/auth/auth.module').then(m=> m.AuthModule)
+  },
+  {
+    path:'**',
+    redirectTo:'auth/login'
   }
 ];
 
