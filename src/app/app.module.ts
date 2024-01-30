69f4b598bc808a904d9343f6971be4bdd2e82e32
Ramiro Bahena <ramiro.bahena@ceneval.edu.mx>
2024-01-30 13:33:34 -0600

    Agregar loading spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './modules/auth/auth.module';
+import { SpinnerComponent } from './modules/shared/spinner/spinner.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoaderInterceptor } from './core/interceptors/loader.interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AuthModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoaderInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/core/interceptors/loader.interceptor.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, finalize } from 'rxjs';
+import { LoaderService } from 'src/app/modules/shared/spinner/loader.service';
+
+@Injectable()
+export class LoaderInterceptor implements HttpInterceptor {
+
+  constructor(private loaderService: LoaderService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    this.loaderService.showLoader();
+    return next.handle(request).pipe(
+      finalize(() => this.loaderService.hideLoader())
+    );
+  }
+}
