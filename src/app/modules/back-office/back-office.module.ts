f35538ddd5fe2ab253142974ade44df1bcb7d215
Ramiro Bahena <ramiro.bahena@ceneval.edu.mx>
2024-02-03 23:36:46 -0600

    Agregar componente de detalle de usuario

--- src/app/modules/back-office/back-office.module.ts
+++ src/app/modules/back-office/back-office.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BackOfficeDashboardComponent } from './back-office-home/back-office-home.component';
 import { BackOfficeRoutingModule } from './back-office-routing.module';
+import { UserDetailComponent } from './navbar/user-detail/user-detail.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    BackOfficeDashboardComponent
+    BackOfficeDashboardComponent,
+    UserDetailComponent
   ],
   imports: [
     CommonModule,
