ad6ccbd5a94d92d57cc1035246e7bc2b6a05590e
rbahena <ramiro.bahena@ceneval.edu.mx>
2024-03-07 22:07:55 -0600

    Agregar metodo para recuperar todas los envases

--- src/app/modules/back-office/catalogs/envases/envases.service.ts
+++ src/app/modules/back-office/catalogs/envases/envases.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.env';
+import { actualizaEnvaseDto, agregarEnvaseDto, envaseDto, obtenDetalleEnvaseDto } from './models/envase.interface';
+import { Observable } from 'rxjs';
+import { category } from '../categories/models/category.interface';
+const USER_LOCAL_STORAGE_KEY_VENDING = 'userData';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnvasesService {
 
-  constructor() { }
+  urlApiBase = environment.urlApi;
+  apiController: string = 'envases';
+
+  constructor(private httpClient: HttpClient) { }
+
+  crearEnvase(crearEnvaseDto: agregarEnvaseDto) { }
+
+  obtenerEnvases(id_suscriptor: number): Observable<envaseDto[]> {
+    const metodApi = 'obtenerEnvase';
+    const urlApi = this.urlApiBase + this.apiController + "/" + metodApi + "/" + id_suscriptor;
+    const token: String = this.loadUserFromLocalStorage();
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    });
+    return this.httpClient.get<envaseDto[]>(urlApi);
+  }
+
+  obtenerEnvase(obtenDetalleEnvaseDto: obtenDetalleEnvaseDto) { }
+
+  actualizaEnvase(actualizaEnvaseDto: actualizaEnvaseDto) { }
+
+  eliminaEnvase(actualizaEnvaseDto: actualizaEnvaseDto) { }
+
+  private loadUserFromLocalStorage(): String {
+    return localStorage.getItem(USER_LOCAL_STORAGE_KEY_VENDING)!;
+  }
 }
