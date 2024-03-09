78a602995aa533c46e137a1549e44d2ebd273653
rbahena <ramiro.bahena@ceneval.edu.mx>
2024-03-08 18:58:10 -0600

    Agregar interfaces para unidades medida produtcos

---
+++ src/app/modules/back-office/catalogs/unidades-medida/models/unidad.producto.interface.ts
@@ -0,0 +1,27 @@
+export interface unidadMedidProductoDto {
+    id_unidad: number;
+    fk_suscriptor: number;
+    nombre_unidad: string;
+    siglas_unidad: string;
+    estatus: number;
+    fecha_alta: Date;
+    fecha_baja: Date;
+}
+
+export interface agregarUnidadMedidProductoDto {
+    fk_suscriptor?: number;
+    nombre_unidad?: string;
+    siglas_unidad: string;
+}
+
+export interface obtenUnidadMedidProductoDto {
+    fk_suscriptor?: number;
+    id_unidad?: number;
+}
+
+export interface actualizaunidadMedidProductoDto{
+    id_unidad?: number;
+    fk_suscriptor?: number;
+    nombre_envase?: string;
+    siglas_unidad?: string;
+}
