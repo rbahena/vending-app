e6359cd97bff372daeed240543df936ef299b81b
rbahena <ramiro.bahena@ceneval.edu.mx>
2024-03-17 16:10:30 -0600

    Agregar componenete para configurar precio

---
+++ src/app/modules/back-office/products/precio-proveedor/precio-proveedor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-precio-proveedor',
+  templateUrl: './precio-proveedor.component.html',
+  styleUrls: ['./precio-proveedor.component.css']
+})
+export class PrecioProveedorComponent {
+
+}
--- src/app/modules/back-office/products/products.module.ts
+++ src/app/modules/back-office/products/products.module.ts
@@ -4,12 +4,14 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../../shared/shared.module';
+import { PrecioProveedorComponent } from './precio-proveedor/precio-proveedor.component';
 
 
 const routes = [{ path: '', component: ProductListComponent }];
 @NgModule({
   declarations: [
-    ProductListComponent
+    ProductListComponent,
+    PrecioProveedorComponent
   ],
   imports: [
     CommonModule, RouterModule.forChild(routes),
