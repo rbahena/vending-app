157c68fa8919e273b1b00d05bf81a534d2983433
rbahena <ramiro.bahena@ceneval.edu.mx>
2024-03-17 11:41:27 -0600

    Agregar pipe para validar esatatus

--- src/app/modules/back-office/products/products.module.ts
+++ src/app/modules/back-office/products/products.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ChecarestatusPipe } from '../catalogs/pipes/checarestatus.pipe';
 
 
 const routes = [{ path: '', component: ProductListComponent }];
 @NgModule({
   declarations: [
+    ChecarestatusPipe,
     ProductListComponent
   ],
   imports: [
